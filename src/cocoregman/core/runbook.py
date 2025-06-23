e7066723bf12335af9b2b6ab39f16f5915e290ce
amutioalex <60644417+amutioalex@users.noreply.github.com>
2025-06-23 10:57:56 +0200

    Extract validation logic to validation.py

--- src/cocoregman/core/runbook.py
+++ src/cocoregman/core/runbook.py
@@ -7,12 +7,9 @@ arguments required for regression management.
 """
 
 from dataclasses import dataclass, field
-from inspect import getfullargspec
-from os.path import expanduser, expandvars
 from pathlib import Path
-from typing import Any, Callable, Dict, List, Union
+from typing import Any, Dict, List
 from warnings import filterwarnings
-from cerberus import Validator
 
 # Suppress warning when importing from cocotb.runner
 filterwarnings("ignore")
@@ -22,153 +19,12 @@ from cocotb.runner import Simulator
 filterwarnings("default")
 
 from yaml import MarkedYAMLError, safe_load, YAMLError
-from cocoregman.core.schema import get_runbook_schema
-from cocoregman.errors import RbFileError, RbValidationError, RbYAMLError
-
-# VALIDATION FUNCTIONS #
-
-
-def _validate_yaml_schema(yaml_dict: dict) -> None:
-    """Validate parsed YAML content against the runbook schema.
-
-    Args:
-        yaml_dict: The YAML dictionary to be validated.
-
-    Raises:
-        RbValidationError: If the provided YAML dictionary does not match the expected
-            schema.
-    """
-    rb_schema = get_runbook_schema("general" in yaml_dict)
-    sch_valid = Validator()
-    if not sch_valid.validate(yaml_dict, rb_schema):
-        raise RbValidationError(0, f"YAML schema validation failed\n{sch_valid.errors}")
-
-
-def _validate_paths(
-    rb_dict: Dict[str, Union[str, int, List[int], Dict[str, Any]]],
-    yaml_path: str,
-) -> None:
-    """Resolve and validate all file system paths in the runbook.
-
-    Ensures that paths specified in the runbook are valid, absolute, and accessible.
-    Additionally, checks for source files referenced by testbenches and whether they
-    are registered under 'srcs'.
-
-    Args:
-        rb_dict: The runbook dictionary after YAML schema validation.
-        yaml_path: Path to runbook YAML file.
-
-    Raises:
-        RbValidationError: If paths are non-existent, unresolved, or incorrectly
-            referenced by testbenches.
-    """
-
-    def get_abs_path(base: str, path: str) -> Path:
-        """Convert a relative or environment-based path to an absolute Path object.
-
-        This function expands user, environment variables and resolves relative paths to
-        absolute paths for consistent file system access. Relative paths are resolved to
-        a provided base path.
-
-        Args:
-            base: Relative path root.
-            path: The string representation of the path to be resolved.
-
-        Returns:
-            An absolute and resolved Path object.
-        """
-        aux_p = Path(expandvars(expanduser(path)))
-        return aux_p if aux_p.is_absolute() else Path(base, str(aux_p))
-
-    # SOURCE PATHS
-    rb_dict["srcs"] = {
-        k: get_abs_path(base=yaml_path, path=v)
-        for k, v in rb_dict.get("srcs", {}).items()
-    }
-
-    # TESTBENCHES
-    for tb_info in rb_dict["tbs"].values():
-        tb_info["path"] = get_abs_path(base=yaml_path, path=tb_info["path"])
-        tb_info["srcs"] = tb_info.get("srcs", [])
-        for args_name in ["build_args", "test_args"]:
-            tb_info[args_name] = {
-                k: expandvars(expanduser(v)) if isinstance(v, str) else v
-                for k, v in tb_info.get(args_name, {}).items()
-            }
-
-    # INCLUDES
-    rb_dict["include"] = [
-        get_abs_path(base=yaml_path, path=i) for i in rb_dict.get("include", [])
-    ]
-
-    # BUILD AND TEST ARGS
-    base_dict = rb_dict.get("general", rb_dict)
-    for args_name in ["build_args", "test_args"]:
-        base_dict[args_name] = {
-            k: expandvars(expanduser(v)) if isinstance(v, str) else v
-            for k, v in base_dict.get(args_name, {}).items()
-        }
-
-    # Check if the provided paths exist, and if they are correctly set
-    x_srcs = list(rb_dict["srcs"].keys())
-    x_non_exist = []
-    x_non_reg = {}
-    for n, path in rb_dict["srcs"].items():
-        if not path.is_file():
-            x_non_exist.append(str(path))
-    for n, tb in rb_dict["tbs"].items():
-        if not tb["path"].is_dir():
-            x_non_exist.append(str(tb["path"]))
-        unreg = [i for i in tb["srcs"] if i not in x_srcs]
-        if unreg:
-            x_non_reg[n] = unreg
-    for path in rb_dict.get("include", []):
-        if not path.exists():
-            x_non_exist.append(str(path))
-
-    if x_non_exist:
-        raise RbValidationError(1, f"Non-existent paths\n{x_non_exist}")
-    if x_non_reg:
-        raise RbValidationError(2, f"Unregistered source indices:\n{x_non_reg}")
-
-
-def _validate_stages_args(args: Dict[str, Any], sim_method: Callable) -> None:
-    """Validate that provided keys are legal arguments for a specific cocotb simulation
-    stage.
-
-    Some keys are excluded from the valid arguments of a stage given they are handled by
-    the cocoman script itself, and user input could potentially disrupt execution flow.
-
-    Args:
-        args: The arguments dictionary.
-        sim_method: The cocotb stage callable.
-
-    Raises:
-        RbValidationError: If the dictionary contains an illegal key.
-    """
-    valid_args = [a for a in getfullargspec(sim_method)[0] if a not in {
-            "self",
-            "verilog_sources",
-            "vhdl_sources",
-            "sources",
-            "hdl_toplevel",
-            "test_module",
-            "hdl_toplevel_lang",
-            "testcase",
-            "always",
-            "timescale",
-        }
-    ]
-    for key in args:
-        if key not in valid_args:
-            raise RbValidationError(
-                3,
-                f"Invalid key '{key}' in '{sim_method.__name__}' arguments. "
-                f"Allowed: {valid_args}",
-            )
-
-
-# CLASS DECLARATION #
+from cocoregman.core.validation import (
+    validate_paths,
+    validate_stages_args,
+    validate_yaml_schema,
+)
+from cocoregman.errors import RbFileError, RbYAMLError
 
 
 @dataclass
@@ -282,16 +138,16 @@ class Runbook:
             raise RbYAMLError(0, excp) from excp
 
         # Validate YAML schema and paths
-        _validate_yaml_schema(rb_dict)
-        _validate_paths(rb_dict=rb_dict, yaml_path=str(file_path.parent))
+        validate_yaml_schema(rb_dict)
+        validate_paths(rb_dict=rb_dict, yaml_path=str(file_path.parent))
 
         rb_dict: dict
         general_dict = rb_dict.get("general", rb_dict)
-        _validate_stages_args(general_dict.get("test_args", {}), Simulator.test)
-        _validate_stages_args(general_dict.get("build_args", {}), Simulator.build)
+        validate_stages_args(general_dict.get("test_args", {}), Simulator.test)
+        validate_stages_args(general_dict.get("build_args", {}), Simulator.build)
         for _, tb_info in rb_dict["tbs"].items():
-            _validate_stages_args(tb_info.get("test_args", {}), Simulator.test)
-            _validate_stages_args(tb_info.get("build_args", {}), Simulator.build)
+            validate_stages_args(tb_info.get("test_args", {}), Simulator.test)
+            validate_stages_args(tb_info.get("build_args", {}), Simulator.build)
 
         return Runbook(
             title=general_dict.get("title", ""),
---
+++ src/cocoregman/core/validation.py
@@ -0,0 +1,146 @@
+# pylint: disable=import-error
+"""Utility functions for validating runbooks, paths, arguments, ..."""
+
+from inspect import getfullargspec
+from os.path import expanduser, expandvars
+from pathlib import Path
+from typing import Any, Callable, Dict, List, Union
+from cerberus import Validator
+from cocoregman.core.schema import get_runbook_schema
+from cocoregman.errors import RbValidationError
+
+
+def validate_yaml_schema(yaml_dict: dict) -> None:
+    """Validate YAML dictionary against the runbook schema.
+
+    Args:
+        yaml_dict: Parsed YAML contents to validate.
+
+    Raises:
+        RbValidationError: If the structure does not match the expected schema.
+    """
+    schema = get_runbook_schema("general" in yaml_dict)
+    sch_valid = Validator()
+    if not sch_valid.validate(yaml_dict, schema):
+        raise RbValidationError(0, f"YAML schema validation failed\n{sch_valid.errors}")
+
+
+def validate_paths(
+    rb_dict: Dict[str, Union[str, int, List[int], Dict[str, Any]]],
+    yaml_path: str,
+) -> None:
+    """Resolve and validate all filesystem paths in the runbook.
+
+    Ensure all relevant paths (sources, testbenches, includes) are valid an exist. Also
+    verifies that testbenches reference only registered source indices.
+
+    Args:
+        rb_dict: Validated runbook dictionary.
+        yaml_path: Path to the runbook YAML file.
+
+    Raises:
+        RbValidationError: If paths are invalid, non-existent, or incorrectly
+            referenced.
+    """
+
+    def get_abs_path(base: str, path: str) -> Path:
+        """Convert a relative or env-based path to an absolute Path object.
+
+        Args:
+            base: Base path for resolving relative paths.
+            path: The string to resolve.
+
+        Returns:
+            An absolute `Path` object.
+        """
+        aux_p = Path(expandvars(expanduser(path)))
+        return aux_p if aux_p.is_absolute() else Path(base) / aux_p
+
+    # SOURCE PATHS
+    rb_dict["srcs"] = {
+        k: get_abs_path(yaml_path, v) for k, v in rb_dict.get("srcs", {}).items()
+    }
+
+    # TESTBENCHES
+    for tb in rb_dict["tbs"].values():
+        tb["path"] = get_abs_path(yaml_path, tb["path"])
+        tb["srcs"] = tb.get("srcs", [])
+        for key in ("build_args", "test_args"):
+            tb[key] = {
+                k: expandvars(expanduser(v)) if isinstance(v, str) else v
+                for k, v in tb.get(key, {}).items()
+            }
+
+    # INCLUDES
+    rb_dict["include"] = [
+        get_abs_path(yaml_path, p) for p in rb_dict.get("include", [])
+    ]
+
+    # BUILD AND TEST ARGS
+    general = rb_dict.get("general", rb_dict)
+    for key in ("build_args", "test_args"):
+        general[key] = {
+            k: expandvars(expanduser(v)) if isinstance(v, str) else v
+            for k, v in general.get(key, {}).items()
+        }
+
+    # Check if the provided paths exist, and if they are correctly set
+    all_src_ind = set(rb_dict["srcs"].keys())
+    missing_paths = []
+    unregistered = {}
+
+    for _, p in rb_dict["srcs"].items():
+        if not p.is_file():
+            missing_paths.append(str(p))
+
+    for ind, tb in rb_dict["tbs"].items():
+        if not tb["path"].is_dir():
+            missing_paths.append(str(tb["path"]))
+        unreg = [i for i in tb["srcs"] if i not in all_src_ind]
+        if unreg:
+            unregistered[ind] = unreg
+
+    for p in rb_dict.get("include", []):
+        if not p.exists():
+            missing_paths.append(str(p))
+
+    if missing_paths:
+        raise RbValidationError(1, f"Non-existent paths\n{missing_paths}")
+    if unregistered:
+        raise RbValidationError(2, f"Unregistered source indices:\n{unregistered}")
+
+
+def validate_stages_args(args: Dict[str, Any], sim_method: Callable) -> None:
+    """Validate that keys in args are valid for a given cocotb simulation stage.
+
+    Ignore common internal arguments that are handled by cocoman itself.
+
+    Args:
+        args: Arguments to validate.
+        sim_method: Simulation stage callable.
+
+    Raises:
+        RbValidationError: If an illegal argument key is detected.
+    """
+    ignored = {
+        "self",
+        "verilog_sources",
+        "vhdl_sources",
+        "sources",
+        "hdl_toplevel",
+        "test_module",
+        "hdl_toplevel_lang",
+        "testcase",
+        "always",
+        "timescale",
+    }
+
+    valid_args = [arg for arg in getfullargspec(sim_method).args if arg not in ignored]
+
+    for key in args:
+        if key not in valid_args:
+            raise RbValidationError(
+                3,
+                f"Invalid key '{key}' in '{sim_method.__name__}' arguments. "
+                f"Allowed: {valid_args}",
+            )
